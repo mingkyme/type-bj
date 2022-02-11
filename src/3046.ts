0c70d4b1f54e228b43994f0f7a55af4e218dc7c8
MINGKYME <admin@mingky.me>
2022-02-11 17:52:38 +0900

    문제 풀이

---
+++ src/3046.ts
@@ -0,0 +1,12 @@
+import fs = require("fs");
+const input: string[] = fs
+    .readFileSync("/dev/stdin")
+    .toString()
+    .trim()
+    .split("\n");
+
+const line1 = input[0].split(' ');
+let n1 = +line1[0];
+let n2 = +line1[1];
+
+console.log(2 * n2 - n1);
