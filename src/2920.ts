7d8433c86bb86300d9200a4b11ef61127d7f60bc
MINGKYME <admin@mingky.me>
2022-02-15 21:18:06 +0900

    문제 풀이

---
+++ src/2920.ts
@@ -0,0 +1,16 @@
+import fs = require("fs");
+const input: string = fs
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")[0];
+
+  if(input == "1 2 3 4 5 6 7 8"){
+    console.log('ascending');
+  }else if(input == "8 7 6 5 4 3 2 1"){
+    console.log('descending');
+
+  }else{
+    console.log('mixed');
+
+  }
