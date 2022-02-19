de5815c04bf7a2ba5432181e22e8483fa3866251
MINGKYME <admin@mingky.me>
2022-02-19 20:07:37 +0900

    문제 풀이

---
+++ src/13277.ts
@@ -0,0 +1,10 @@
+import fs = require("fs");
+const input: string[] = fs
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim().split('\n');
+
+let n1 :bigint = BigInt(input[0].split(' ')[0]);
+let n2 :bigint = BigInt(input[0].split(' ')[1]);
+
+console.log((n1*n2).toString())
