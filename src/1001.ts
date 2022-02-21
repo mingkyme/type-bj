4887714e53df687f85a00a63519d63e0c69ae10a
MINGKYME <admin@mingky.me>
2022-02-21 19:29:53 +0900

    js로 구현 돼 있는 파일 수정

--- src/1001.ts
+++ src/1001.ts
@@ -1,11 +1,8 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const fs = require("fs");
-const input = fs
+import fs = require("fs");
+const input: string[] = fs
     .readFileSync("/dev/stdin")
     .toString()
-    .trim()
-    .split("\n")[0].split(' ');
+    .trim().split('\n')[0].split(' ');
 let n1 = +input[0];
 let n2 = +input[1];
 console.log(n1 - n2);
