adce136dc17a793d4b67cd45804b501d3cf6579d
MINGKYME <admin@mingky.me>
2022-02-27 11:33:31 +0900

    문제 풀이

---
+++ src/15894.ts
@@ -0,0 +1,9 @@
+import fs = require("fs");
+let input : string[];
+if(process.platform == 'linux'){
+    input = fs.readFileSync("/dev/stdin").toString().trim().split('\n');
+}else{
+    input = `1`.split('\n');
+}
+
+console.log(+input[0] * 4)
