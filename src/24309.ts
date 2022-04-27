8a07e531b8f70c64ce31c2371f6622342b542807
MINGKYME <admin@mingky.me>
2022-04-27 13:20:20 +0900

    문제 풀이

--- src/24309.ts
+++ src/24309.ts
@@ -1,14 +1,14 @@
 import fs = require("fs");
-let input : string[];
-if(process.platform == 'linux'){
+let input: string[];
+if (process.platform == 'linux') {
     input = fs.readFileSync("/dev/stdin").toString().trim().split('\n');
-}else{
+} else {
     input = `4
 20
 8`.split('\n');
 }
-let a = +input[0];
-let b = +input[1];
-let c = +input[2];
+let a = BigInt(input[0]);
+let b = BigInt(input[1]);
+let c = BigInt(input[2]);
 
-console.log( (b-c) / a);
+console.log(((b - c) / a).toString());
